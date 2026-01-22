2250064382cbe5e26af89366bc49733d953d5505
kame no oshiri <chng.sean2@gmail.com>
2026-01-22 09:38:18 +0800

    refactor: Changed name of bot from "Duke" to "Bill"
    Removed all traces of "Duke" and changed all instances of "Duke" to "Bill" in source code; updated "EXPECTED.TXT" to reflect change in bot name;

---
+++ src/main/java/Bill.java
@@ -0,0 +1,8 @@
+public class Bill {
+    public static void main(String[] args) {
+        String name = "Bill";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
