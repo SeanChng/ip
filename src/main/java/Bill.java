f34eff7d6db220afdbc2066cab37411b47f950c0
kame no oshiri <chng.sean2@gmail.com>
2026-01-26 18:05:25 +0800

    feat: add marking tasks as done or not done on list view
    Add ability to mark tasks in list of tasks as done or not done according to list index; add echo when marking and unmarking tasks

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -6,7 +6,7 @@ public class Bill {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int tasksCnt = 0;
 
         String line;
@@ -15,11 +15,25 @@ public class Bill {
 
         while (!(line.equals("bye"))) {
             if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasksCnt; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i]);
+                    System.out.println(i + 1 + ". " +
+                                               tasks[i].getStatusIcon() + " " +
+                                               tasks[i].description);
                 }
+            } else if (line.matches("^mark\\s([1-9][0-9]?)")) {
+                int idx = Integer.parseInt(line.substring(5)) - 1;
+                tasks[idx].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[idx].getStatusIcon() + " " + tasks[idx].description);
+            } else if (line.matches("^unmark\\s([1-9][0-9]?)")) {
+                int idx = Integer.parseInt(line.substring(7)) - 1;
+                tasks[idx].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[idx].getStatusIcon() + " " + tasks[idx].description);
             } else {
-                tasks[tasksCnt] = line;
+                Task newTask = new Task(line);
+                tasks[tasksCnt] = newTask;
                 tasksCnt++;
                 System.out.println("added: " + line);
             }
