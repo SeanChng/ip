9e81e89cbbd01433ea96ac3d25f5315a6c384d9b
kame no oshiri <chng.sean2@gmail.com>
2026-01-26 17:33:26 +0800

    feat: store user input and echo when requested
    Add the ability to store whatever text entered by the user and display
    them back to the user when requested

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -6,12 +6,24 @@ public class Bill {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
 
+        String[] tasks = new String[100];
+        int tasksCnt = 0;
+
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
 
         while (!(line.equals("bye"))) {
-            System.out.println(line);
+            if (line.equals("list")) {
+                for (int i = 0; i < tasksCnt; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+            } else {
+                tasks[tasksCnt] = line;
+                tasksCnt++;
+                System.out.println("added: " + line);
+            }
+
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
