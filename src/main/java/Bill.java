9ec61cc596244e01e5afc76a677a6831c573256f
kame no oshiri <chng.sean2@gmail.com>
2026-02-05 18:50:36 +0800

    refactor(Bill): create MessageParser.java, refactor if-else block into switch-case block
    Add MessageParser.java to handle splitting user input into type of task
    and description of task

--- src/main/java/Bill.java
+++ src/main/java/Bill.java
@@ -2,12 +2,7 @@ import java.util.Scanner;
 
 public class Bill {
     public static void main(String[] args) {
-        String NAME = "Bill";
-        int MARK_INDEX = 5;
-        int UNMARK_INDEX = 7;
-        int TODO_INDEX = 5;
-        int DEADLINE_INDEX = 9;
-        int EVENT_INDEX = 6;
+        final String NAME = "Bill";
 
         System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
@@ -16,37 +11,39 @@ public class Bill {
 
         String userInput;
         Scanner in = new Scanner(System.in);
-        userInput = in.nextLine();
 
-        while (!(userInput.equals("bye"))) {
-            userInput = userInput.trim();
+        while (true) {
+            userInput = in.nextLine().trim();
+            if (userInput.equals("bye")) {
+                break;
+            }
             if (userInput.equals("list")) {
                 manager.listTasks();
-            } else if (userInput.startsWith("mark")) {
-                String taskPart = userInput.substring(MARK_INDEX);
-                int taskIndex = Integer.parseInt(taskPart);
-                manager.markTask(taskIndex - 1, true);
-            } else if (userInput.startsWith("unmark")) {
-                String taskPart = userInput.substring(UNMARK_INDEX);
-                int taskIndex = Integer.parseInt(taskPart);
-                manager.markTask(taskIndex - 1, false);
-            } else if (userInput.startsWith("todo")) {
-                manager.addTask(new ToDo(userInput.substring(TODO_INDEX)));
-            } else if (userInput.startsWith("deadline")) {
-                int byIndex = userInput.indexOf("/by");
-                manager.addTask(
-                        new Deadline(userInput.substring(DEADLINE_INDEX, byIndex - 1),
-                                     userInput.substring(byIndex + 4)));
-            } else if (userInput.startsWith("event")) {
-                int startIndex = userInput.indexOf("/from");
-                int endIndex = userInput.indexOf("/to");
-                manager.addTask(new Event(userInput.substring(EVENT_INDEX, startIndex - 1),
-                                          userInput.substring(startIndex + 6, endIndex - 1),
-                                          userInput.substring(endIndex + 4)));
             }
-            userInput = in.nextLine();
+
+            MessageParser msg = new MessageParser(userInput);
+            switch (msg.getTaskType()) {
+            case "mark":
+                manager.markTask(msg.getMarkedIndex(), true);
+                break;
+            case "unmark":
+                manager.markTask(msg.getMarkedIndex(), false);
+                break;
+            case "todo":
+                manager.addTask(new ToDo(msg.getTodoDescription()));
+                break;
+            case "deadline":
+                manager.addTask(new Deadline(msg.getDeadlineDescription()[0],
+                                             msg.getDeadlineDescription()[1]));
+                break;
+            case "event":
+                manager.addTask(new Event(msg.getEventDescription()[0],
+                                          msg.getEventDescription()[1],
+                                          msg.getEventDescription()[2]));
+                break;
+            }
         }
-        System.out.println("Bye. Hope to see you again soon!");
         in.close();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
---
+++ src/main/java/MessageParser.java
@@ -0,0 +1,52 @@
+public class MessageParser {
+
+    private final String taskType;
+    private final String[] components;
+    private int markedIndex;
+    private String todoDescription;
+    private String[] deadlineDescription;
+    private String[] eventDescription;
+
+    public MessageParser(String userInput) {
+        components = userInput.split(" ", 2);
+        taskType = components[0];
+        switch (taskType) {
+        case "mark":
+        case "unmark":
+            markedIndex = Integer.parseInt(components[1]);
+            break;
+        case "todo":
+            todoDescription = components[1];
+            break;
+        case "deadline":
+            deadlineDescription = components[1].split(" /by ", 2);
+            break;
+        case "event":
+            eventDescription = components[1].split(" /from | /to ", 3);
+        }
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public String[] getComponents() {
+        return components;
+    }
+
+    public String getTodoDescription() {
+        return todoDescription;
+    }
+
+    public String[] getDeadlineDescription() {
+        return deadlineDescription;
+    }
+
+    public String[] getEventDescription() {
+        return eventDescription;
+    }
+
+    public int getMarkedIndex() {
+        return markedIndex;
+    }
+}
