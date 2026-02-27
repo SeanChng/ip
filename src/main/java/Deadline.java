b249a8bc6311e07dea1b9af36666576138ba51a3
kame no oshiri <chng.sean2@gmail.com>
2026-02-27 17:08:29 +0800

    feat!: implement LocalDateTime for Deadline and Event
    Switch from raw String storage to java.time.LocalDateTime to
    allow for date validation and custom formatting
    
    BREAKING CHANGE: Existing tasks.txt files using the old string
    format will cause a parsing error on load

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,20 @@
-public class Deadline extends Task {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    protected String by;
+public class Deadline extends Task {
+    protected LocalDateTime by;
+    private static final DateTimeFormatter INPUT_FORMAT =
+            DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
 
     public Deadline(String description, String by) {
         super(description);
+        this.by = LocalDateTime.parse(by.trim(), INPUT_FORMAT);
+    }
+
+    public Deadline(String description, LocalDateTime by) {
+        super(description);
         this.by = by;
     }
-
-    public String getBy() {
-        return by;
-    }
-
 
     @Override
     public String toSaveLine() {
@@ -19,8 +23,7 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
     }
 }
-
-
